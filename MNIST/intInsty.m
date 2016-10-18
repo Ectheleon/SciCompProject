e40d48778b8a1c2f3fbb6e8cbad8d8631b3c808c
Jonathan Peters <peters@maths.ox.ac.uk>
2016-10-18 15:34:30 +0100

    updated this function

--- MNIST/intInsty.m
+++ MNIST/intInsty.m
@@ -1,9 +1,9 @@
 function [mu,trainFt]=intInsty(images)
 % Intense Feature 
 % This script calculated the mean image of the MNIST training set. 
-meanIm=(mean(images'))';
+mu=(mean(images'))';
 % Find the variance of each of the images 
-trainFt=images-meanIm;
+trainFt=images-mu;
 end 
 
 
